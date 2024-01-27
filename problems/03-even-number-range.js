4f8d6604fef7eb4f6ea910dc9293384920dc1b6f
Scott Feichter <scottfeichter@gmail.com>
2024-01-26 18:12:01 -0800

    03 finished

--- problems/03-even-number-range.js
+++ problems/03-even-number-range.js
@@ -1,20 +1,41 @@
 /* Even Number Range
 
-    Write a function that accepts two numbers. 
-    The function should return an array of all even numbers between the two arguments inclusively.
+    Write a function that accepts two numbers.
+    The function should return an array of all even numbers between the two
+    arguments inclusively.
 
     console.log(evenNumberRange(-3, 2)); // prints [-2, 0, 2]
     console.log(evenNumberRange(22, 25)); // prints [22, 24]
     console.log(evenNumberRange(2, 0)); // prints []
 */
 
-function evenNumberRange(num1, num2) {
-    // Your code here 
+function evenNumberRange(num1, num2, nums = []) {
+  if (num1 > num2) {
+    return nums;
+  }
+
+  if (num1 === num2) {
+    if (num1 % 2 === 0) {
+      nums.push(num1);
+    }
+    return nums;
+  }
+
+  if (num1 < num2) {
+    if (num1 % 2 === 0) {
+      let newNum = num1;
+      nums.push(newNum);
+    }
+    num1++;
+    return evenNumberRange(num1, num2, nums);
+  }
+
+  return "something went wrong";
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
-    module.exports = evenNumberRange;
+  module.exports = evenNumberRange;
 } catch (e) {
-    module.exports = null;
+  module.exports = null;
 }
