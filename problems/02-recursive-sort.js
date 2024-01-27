dd7ef71a715b47beae36f1a1d835ccf036ed98e7
Scott Feichter <scottfeichter@gmail.com>
2024-01-26 17:26:44 -0800

    pausing

--- problems/02-recursive-sort.js
+++ problems/02-recursive-sort.js
@@ -1,21 +1,36 @@
 /*
     Recursive Sort
 
-    Given an array of numbers, write a function that returns a new array of numbers sorted from lowest to highest.
+    Given an array of numbers, write a function that returns a new array of
+    numbers sorted from lowest to highest.
 
     console.log(sort([])) // prints []
     console.log(sort([9])) // prints [9]
     console.log(sort([5, 4, 3, 2, 1])) // prints [1, 2, 3, 4, 5]
-    console.log(sort([14, 5, 10, 6, 3, 4, 21, 16, 9])); // prints [ 3, 4, 5, 6, 9, 10, 14, 16, 21 ]
+    console.log(sort([14, 5, 10, 6, 3, 4, 21, 16, 9]));
+    // prints [ 3, 4, 5, 6, 9, 10, 14, 16, 21 ]
 */
 
-function sort(nums) {
-    // Your code here 
+function sort(nums, nuArr = []) {
+  debugger;
+  if (nums.length === 0) return nuArr;
+  for (let i = 1; i < nums.length; i++) {
+    if(nums[0])
+  }
+
+  return sort(nums, nuArr);
+  return "something went wrong";
 }
+
+// console.log(sort([])) // prints []
+//     console.log(sort([9])) // prints [9]
+console.log(sort([5, 4, 3, 2, 1])); // prints [1, 2, 3, 4, 5]
+// console.log(sort([14, 5, 10, 6, 3, 4, 21, 16, 9]));
+// prints [ 3, 4, 5, 6, 9, 10, 14, 16, 21 ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
-    module.exports = sort;
+  module.exports = sort;
 } catch (e) {
-    module.exports = null;
+  module.exports = null;
 }
